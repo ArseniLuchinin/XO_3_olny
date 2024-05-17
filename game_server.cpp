6ebca0c9991c4a6ed91d9a2829638fad95792d3c
Arseni Luchinin <143221669+ArseniLuchinin@users.noreply.github.com>
2024-05-17 21:50:28 +0300

    Start develop server

---
+++ game_server.cpp
@@ -0,0 +1,6 @@
+#include "game_server.h"
+
+Game_server::Game_server()
+{
+
+}
---
+++ game_server.h
@@ -0,0 +1,12 @@
+#ifndef GAME_SERVER_H
+#define GAME_SERVER_H
+
+
+class Game_server
+{
+    Q_OBJECT
+public:
+    Game_server();
+};
+
+#endif // GAME_SERVER_H
