1113b5d715703bb713a2ce7da61a4b3bc68f7b13
Arseni Luchinin <143221669+ArseniLuchinin@users.noreply.github.com>
2024-05-22 19:49:54 +0300

    Server read client reqest and sent datas

--- game_server.cpp
+++ game_server.cpp
@@ -18,16 +18,29 @@ void Game_server::incomingConnection(qintptr handle){
     connect(socket, &QTcpSocket::readyRead, this, &Game_server::read_request);
     connect(socket, &QTcpSocket::disconnected, socket, &QTcpSocket::deleteLater);
 
-    QByteArray d;
-
-    QDataStream to_client{&d, QIODevice::WriteOnly};
-    to_client.setVersion(QDataStream::Qt_5_9);
-    to_client << "con";
-    socket->write(d);
     sockets.append(QSharedPointer<QTcpSocket>{socket});
     qDebug() << "New connect: " << socket->socketDescriptor();
+
+}
+void Game_server::read_request(){
+    socket = (QTcpSocket*)(sender());
+    QDataStream in{socket};
+    in.setVersion(QDataStream::Qt_5_9);
+    if(in.status() == QDataStream::Ok){
+        qint32 new_pos;
+        in >> new_pos;
+        send_data_positoin_to_client(new_pos, 1);
+    }
+    else{
+        qDebug() << "Client Date Error\n";
+    }
 }
 
-void Game_server::read_request(){
-
+void Game_server::send_data_positoin_to_client(const qint32 new_pos, const qint32 del_pos){
+    socket = sockets[0].data();
+    data.clear();
+    QDataStream out{&data, QIODevice::WriteOnly};
+    out.setVersion(QDataStream::Qt_5_9);
+    out << new_pos;
+    socket->write(data);
 }
--- game_server.h
+++ game_server.h
@@ -9,6 +9,7 @@
 class Game_server : public QTcpServer
 {
     Q_OBJECT
+    void send_data_positoin_to_client(const qint32, const qint32);
 protected:
     QTcpSocket *socket;
     QList<QSharedPointer<QTcpSocket>> sockets;
